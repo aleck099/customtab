12fc948a95fbbf0d39c1248a919e6b583e16d579
aleck099 <aleck099@qq.com>
2022-01-30 17:46:58 +0800

    Setup

---
+++ src/main/java/net/accel/customtab/ModMain.java
@@ -0,0 +1,15 @@
+package net.accel.customtab;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModMain implements ModInitializer {
+    public static final String ID = "customtab";
+    public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
